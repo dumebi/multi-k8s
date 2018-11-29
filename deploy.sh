82ad6cb5df0a33b5b759b1cf53c401b869337152
Jude Dike <dikejude49@gmail.com>
2018-11-29 01:31:31 +0100

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 docker build -t dyke2010/multi-client:latest -t dyke2010/multi-client:$SHA ./client/Dockerfile ./client
 docker build -t dyke2010/multi-server:latest -t dyke2010/multi-server:$SHA ./server/Dockerfile ./server
 docker build -t dyke2010/multi-worker:latest -t dyke2010/multi-worker:$SHA ./worker/Dockerfile ./worker
+
 docker push dyke2010/multi-client:latest
 docker push dyke2010/multi-server:latest
 docker push dyke2010/multi-worker:latest
+
 docker push dyke2010/multi-client:$SHA
 docker push dyke2010/multi-server:$SHA
 docker push dyke2010/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=dyke2010/multi-server:$SHA
 kubectl set image deployments/client-deployment client=dyke2010/multi-client:$SHA
